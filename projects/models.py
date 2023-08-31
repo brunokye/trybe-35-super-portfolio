1ab30fc5782ccdbeecf8c80ff4bf600f07164bbb
Bruno Ferreira <contatobrunokye@gmail.com>
2023-08-31 14:40:25 -0300

    feat: requisito 05

--- projects/models.py
+++ projects/models.py
@@ -18,7 +18,34 @@ class Project(models.Model):
     keyword = models.CharField(max_length=50)
     key_skill = models.CharField(max_length=50)
     profile = models.ForeignKey(
-        Profile, on_delete=models.CASCADE, related_name="projects"
+        Profile,
+        on_delete=models.CASCADE,
+        related_name="projects"
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class CertifyingInstitution(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.URLField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
+class Certificate(models.Model):
+    name = models.CharField(max_length=100)
+    certifying_institution = models.ForeignKey(
+        CertifyingInstitution,
+        on_delete=models.CASCADE,
+        related_name="certificates"
+    )
+    timestamp = models.DateTimeField(auto_now_add=True)
+    profiles = models.ManyToManyField(
+        Profile,
+        related_name="certificates"
     )
 
     def __str__(self):
--- projects/serializers.py
+++ projects/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Profile, Project
+from .models import Profile, Project, CertifyingInstitution, Certificate
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -11,4 +11,38 @@ class ProfileSerializer(serializers.ModelSerializer):
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
+        fields = '__all__'
+
+
+class NestedCertificateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Certificate
+        fields = ['id', 'name']
+
+
+class CertifyingInstitutionSerializer(serializers.ModelSerializer):
+    certificates = NestedCertificateSerializer(many=True)
+
+    class Meta:
+        model = CertifyingInstitution
+        fields = ['id', 'name', 'url', 'certificates']
+
+    def create(self, validated_data):
+        data = validated_data.pop('certificates')
+        certifying_institution = CertifyingInstitution.objects.create(
+            **validated_data
+        )
+
+        for certificate in data:
+            Certificate.objects.create(
+                certifying_institution=certifying_institution,
+                **certificate
+            )
+
+        return certifying_institution
+
+
+class CertificateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Certificate
         fields = '__all__'
